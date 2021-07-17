e3bd6b75d8169a4438e60b214a7a9a16fd105757
naidu3009 <naidu3009@gmail.com>
2021-07-17 17:54:35 +0530

    maven

---
+++ src/main/java/org/ntpc/sample.java
@@ -0,0 +1,13 @@
+package org.ntpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class sample 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
