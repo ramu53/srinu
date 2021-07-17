6f721f12d8659f71a0e8b0421cefa37238ae171f
naidu3009 <naidu3009@gmail.com>
2021-07-17 07:12:01 +0530

    first commit

---
+++ src/main/java/org/ntpc/App.java
@@ -0,0 +1,13 @@
+package org.ntpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
